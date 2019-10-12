111631fa95de98bb6310d19ddd55aac2568ed37f
hussein al saadi <hiso3d@gmail.com>
2019-10-12 18:22:27 +0300

    fixing React not found error

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-import { NativeModules ,requireNativeComponent ,Platform } from 'react-native';
+import React from "react"
+import { NativeModules, requireNativeComponent, Platform } from 'react-native';
 
 const { AppleSignIn } = NativeModules;
 
 export const RNSignInWithAppleButton = requireNativeComponent('RNCSignInWithAppleButton');
 
 export const SignInWithAppleButton = (buttonStyle, callBack) => {
-  if(Platform.OS === 'ios'){
+  if (Platform.OS === 'ios') {
     return <RNSignInWithAppleButton style={buttonStyle} onPress={async () => {
-    
-        await RNCAppleAuthentication.requestAsync({
-          scopes: [RNCAppleAuthentication.Scope.FULL_NAME, RNCAppleAuthentication.Scope.EMAIL],
-        }).then((response) => {
-          callBack(response) //Display response
-          }, (error) => {
-            callBack(error) //Display error
-           
-        });
+      await RNCAppleAuthentication.requestAsync({
+        scopes: [RNCAppleAuthentication.Scope.FULL_NAME, RNCAppleAuthentication.Scope.EMAIL],
+      }).then((response) => {
+        callBack(response) //Display response
+      }, (error) => {
+        callBack(error) //Display error
+      });
 
-  }} />
-  }else{
-  return null
+    }} />
+  } else {
+    return null
 
   }
-   
+
 }
 
 
